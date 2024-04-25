4643db0e1ee5621e5ad4674315d58eb1efdd635d
Miguel Angel <miguel.lopezm.dev@gmail.com>
2024-04-25 00:47:55 -0600

    Added custom users model

---
+++ apps/users/managers.py
@@ -0,0 +1,83 @@
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager, models.Manager):
+    """Manager for new user model
+
+    Args:
+        BaseUserManager (obj): class Base of the user Manager of Django
+        models (obj): class of the model of Manager of the user of Django
+    """
+
+    def _create_user(
+        self,
+        username,
+        email,
+        password,
+        is_staff,
+        is_superuser,
+        is_active,
+        **extra_fields,
+    ):
+        """Private method for create an user based in new model user
+
+        Args:
+            username (str): knick name
+            email (str): email
+            password (str): password
+            is_staff (bool): the user has permisssions for the admin panel?
+            is_superuser (bool): the user is an admin?
+            is_active (bool): status of the user
+            **extra_fields (dict): other fields
+
+        Returns:
+            obj: returns an user instance
+        """
+        # Create an user instance
+        user = self.model(
+            username=username,
+            email=email,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            is_active=is_active,
+            **extra_fields,
+        )
+
+        user.set_password(password)  # Encrypt the password
+        user.save(using=self.db)  # Save password encrypted
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        """Method for create un normal user
+
+        Args:
+            username (_type_): _description_
+            email (_type_): _description_
+            password (_type_, optional): _description_. Defaults to None.
+            **extra_fields (dict): other fields
+
+
+        Returns:
+            obj: The private method for create an user
+        """
+        return self._create_user(
+            username, email, password, False, False, True, **extra_fields
+        )
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """Method for create a super user
+
+        Args:
+            username (str): username
+            email (str): email
+            password (str, optional): password. Defaults to None.
+            **extra_fields (dict): other fields
+
+
+        Returns:
+            obj: the private method for create an super user
+        """
+        return self._create_user(
+            username, email, password, True, True, True, **extra_fields
+        )
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from apps.abstracts.models import AbstractModel
+
+from .managers import UserManager
+
+
+class User(PermissionsMixin, AbstractBaseUser, AbstractModel):
+    """
+      It is a new model for custom user
+    Args:
+        username ( str ): knick name.
+        email ( str ): email of the user
+        is_staff ( bool ): is an user with permissions of the admin panel?
+        name ( str ): name of the user
+        last_name ( str ): last name of the user
+        age ( int ): age of the user
+        phone ( str ): phone number of the user
+        address ( str ): address of the user
+
+    """
+
+    username = models.CharField(max_length=10, null=False, unique=True)
+    email = models.EmailField(unique=True, null=False)
+    is_staff = models.BooleanField(default=False)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(null=False, blank=False)
+    phone = models.CharField(max_length=10, null=True, blank=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
